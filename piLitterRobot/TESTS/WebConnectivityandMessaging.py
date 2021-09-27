3a4b31c4863a1d19248c2c1ee59d3eef327ffc05
kaiju466 <kaiju466@gmail.com>
2021-09-27 09:22:28 -0500

    updated test

--- piLitterRobot/TESTS/WebConnectivityandMessaging.py
+++ piLitterRobot/TESTS/WebConnectivityandMessaging.py
@@ -7,6 +7,10 @@ receiver_email = "kaiju466@gmail.com"
 password = "Ezekiel!180"#input("Type your password and press enter:")
 subject="Subject:"
 
+cycle_count=1
+cycle_num_max=4
+numInterval_Hours=6
+
 context = ssl.create_default_context()
 def notify(sbj,msg):
     with smtplib.SMTP(smtp_server, port) as server:
@@ -17,8 +21,9 @@ def notify(sbj,msg):
         sbj=subject+sbj
         server.sendmail(sender_email, receiver_email, sbj+" \n"+msg)
 
-notify("besttest","testing 123")
-    
+#notify("besttest","testing 123")
+notify("Starting piLitterRobot","piLitterRobot has started it's run cycle. Will cycle "+str(cycle_num_max)+" times every "+str(numInterval_Hours)+" hours")
+   
     
     
     
