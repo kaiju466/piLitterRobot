ecf7ab131c7667b4e972539a08f355b958579c29
Josh <kaiju466@gmail.com>
2021-10-22 10:05:33 -0500

    update

--- piLitterRobot/TESTS/eStop.py
+++ piLitterRobot/TESTS/eStop.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 from waveshare import MotorDriver
-
+import time
 
 prog_version = 1.0
-prog_name = "Waveshare Test Program"
+prog_name = "Waveshare Estop Program"
 
 # Title Screen
 print("---------------------------------")
@@ -16,19 +16,7 @@ time.sleep(2.00)
 
 print("Start Test")
 
-print("this is a motor driver test code")
 Motor = MotorDriver()
-
-print("forward 2 s")
-Motor.MotorRun(0, 'forward', 100)
-Motor.MotorRun(1, 'forward', 100)
-time.sleep(2)
-
-print("backward 2 s")
-Motor.MotorRun(0, 'backward', 100)
-Motor.MotorRun(1, 'backward', 100)
-time.sleep(2)
-
 print("stop")
 Motor.MotorStop(0)
 Motor.MotorStop(1)
